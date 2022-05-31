81272c75eb96feef9db1441e1c72edc689d0f34b
iydia <lydia.he23@ycdsbk12.ca>
2022-05-31 14:17:18 -0400

    collision detection

--- Sketch1.java
+++ Sketch1.java
@@ -1,32 +1,89 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch1 extends PApplet {
-	
+
+	/**
+   * Assignment: 6.1 Processing in Java CPT
+   * Author: Lydia He, Parker Yang
+   * Due Jun 13, 11:59 PM
+   * Description: WHACK-A-MOLE
+   */
+
+  PImage imgBackground;
+  PImage imgOfficer1;
+  PImage imgOfficer2;
+  PImage imgOfficer3;
+  PImage imgOfficer4;
+  float officerX = 350;
+  float officerY = 600;
+  float officerXspeed = 3;
+  float officerYspeed = 3;
+
   public void settings() {
 	// put your size call here
     size(1300, 700);
+
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    // Import bg
+    imgBackground = loadImage("ground_04.png");
+    imgOfficer1 = loadImage("player_01.png");
+    imgOfficer2 = loadImage("player_03.png");
+    imgOfficer3 = loadImage("player_09.png");
+    imgOfficer4 = loadImage("player_012.png");
+
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Draws background
+    for(int intRow = 0; intRow < 1300; intRow+=125){
+      for(int intColumn = 0; intColumn <= 700; intColumn+=125){
+        image(imgBackground, intRow, intColumn);
+      }
+    }
+     // Officer
+     image(imgOfficer1, officerX, officerY);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+     // Move up
+     if (keyPressed) {
+       if (keyCode == UP) {
+         image(imgOfficer1, officerX, officerY);
+         officerY -= 3;
+     }
+   }
+ 
+     // Move down
+     if (keyPressed) {
+       if (keyCode == DOWN) {
+         officerY += 3;
+   }
   }
-  
-  // define other methods down here.
-}
+    // Move left
+    if (keyPressed) {
+     if (keyCode == LEFT) {
+       officerX -= 3;
+  }
+ }
+     // Move right
+     if (keyPressed) {
+      if (keyCode == RIGHT) {
+       officerX += 3;
+    }
+   } 
+
+  // Collision Detection
+  officerX += officerXspeed;
+  officerY += officerYspeed;
+
+  if (officerX < 0 || officerX > width) {
+    officerXspeed *= -1;
+  }
+
+  if (officerY < 0 || officerY > height) {
+    officerYspeed *= -1;
+  }
+
+  }
+ }
