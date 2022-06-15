a74d47c6de13a919d893a5abab1b9747e5b9aea6
iydia <lydia.he23@ycdsbk12.ca>
2022-06-15 09:50:17 -0400

    bottom of jail collision done

--- Lydias_tester_file.java
+++ Lydias_tester_file.java
@@ -404,7 +404,9 @@ public class Lydias_tester_file extends PApplet {
     // Right exit: from (7, 16) going right 7 tiles
     || ((officerY <= TILE_HEIGHT * 8) && (officerY >= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 16))
     // Top of jail cell from (6, 9) going right 3 tiles
-    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13))
+    // Bottom of jail cell from (10, 9) going right 3 tiles
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 10) && (officerY <= TILE_HEIGHT * 11) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
     { 
       officerY += intOfficerSpeed;
     }
@@ -420,7 +422,9 @@ public class Lydias_tester_file extends PApplet {
     // from (7, 16) going right 5 tiles
     || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 7) && (officerY <= TILE_HEIGHT * 8) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 16))
     // Top of jail cell from (6, 9) going right 3 tiles
-    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13))
+    // Bottom of jail cell from (10, 9) going right 3 tiles
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 10) && (officerY <= TILE_HEIGHT * 11) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
     { 
       officerY -= intOfficerSpeed;
     }
@@ -432,7 +436,9 @@ public class Lydias_tester_file extends PApplet {
     // Left exit: from (7, 1) going right 3 tiles
     || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 7) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 8) && (officerX <= TILE_WIDTH * 3))
     // Top of jail cell from (6, 9) going right 3 tiles
-    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13))
+    // Bottom of jail cell from (10, 9) going right 3 tiles
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 10) && (officerY <= TILE_HEIGHT * 11) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
     {
       officerX += intOfficerSpeed;
     }
@@ -444,7 +450,9 @@ public class Lydias_tester_file extends PApplet {
     // Right exit: from (7, 16) going right 7 tiles
     || ((officerY <= TILE_HEIGHT * 8) && (officerY >= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 16))
     // Top of jail cell from (6, 9) going right 3 tiles
-    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 6) && (officerY + PLAYER_HEIGHT <= TILE_HEIGHT * 7) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13))
+    // Bottom of jail cell from (10, 9) going right 3 tiles
+    || ((officerY + PLAYER_HEIGHT >= TILE_HEIGHT * 10) && (officerY <= TILE_HEIGHT * 11) && (officerX + PLAYER_WIDTH >= TILE_WIDTH * 9) && (officerX + PLAYER_WIDTH/2 <= TILE_WIDTH * 13)))
     {
       officerX -= intOfficerSpeed;
     }
