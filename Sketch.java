35dc96c99b87220129927925964e4ba425c4e764
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-06-15 10:25:25 -0400

    Changed time limit

--- Sketch.java
+++ Sketch.java
@@ -55,7 +55,7 @@ public class Sketch extends PApplet {
   int intCrateY;
   int intOfficer_frames = 8;
   int intArrayValue;
-  int intTimer = 120000;
+  int intTimer = 3529; // Around 1 minute
   int intKeyCount = 0;
   int intTimerStart; 
   int[][] intArray;
